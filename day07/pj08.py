e8fb709a564ddb0b2f5de1835a8bd50f0a8efbed
RiverGang <sory304@naver.com>
2024-07-01 17:22:09 +0900

    Update pj08.py

--- day07/pj08.py
+++ day07/pj08.py
@@ -12,11 +12,14 @@ ledPin= {"Red":18, "Blue":23, "Green":24} # LED GPIO핀 딕셔너리 형태
 piezoPin = 12
 switch = 21
 
+## 전역변수 선언
 rpiTimeNow = ""
 userSetTime = None
 userSetLED = None
 userSetRepeat = None
 userSetSound = None
+
+# GPIO 세팅
 
 GPIO.setmode(GPIO.BCM)
 # LED setting
@@ -57,7 +60,7 @@ class WindowClass (QMainWindow, form_class):
 		super().__init__() # 부모클래스 생성자 (QWidges)
 		self.setupUi(self)
 
-		# QTimer 설정
+		# QTimer 설정 실시간 시간 업데이트
 		self.timer = QTimer(self)
 		self.timer.timeout.connect(self.update_time)
 		self.timer.start(1000) # 1초마다 갱신
@@ -67,33 +70,34 @@ class WindowClass (QMainWindow, form_class):
 	def update_time(self):
 			global rpiTimeNow, userSetTime
 
-			timestamp = time.time()
+			timestamp = time.time() ## 현재 시간 가져오기
 			lt = time.localtime(timestamp)
-			timeNow = time.strftime("%H:%M", lt)
+			timeNow = time.strftime("%H:%M", lt) ## 문자형태 변환
 			self.lcdNumber.display(timeNow)
 
 			rpiTimeNow  = time.strftime("%H%M", lt) # 라즈베리 파이(콘솔)로 넘길 시간
 
 			if userSetTime is not None and userSetTime == rpiTimeNow:
-				LED_blink()
-				userSetTime = None
+				LED_blink() ## 알람실행
 
 	def BtnsaveFnc(self):
 			t = self.timeEdit.time() # timeEdit 시간  가져오기
 
+			## 사용자가 설정한 정보 전역 변수에 담기
 			global userSetTime, userSetLED, userSetRepeat, userSetSound
 			userSetTime = t.toString("HHmm")
 			userSetLED = ledPin[self.CboLED.currentText()]
 			userSetRepeat = int(self.CboRepeat.currentText())
 			userSetSound = self.CboSound.currentText()
 
+			## 하단 알람설정 정보창 업데이트
 			self.LblTime.setText("Time : " + userSetTime)
-			self.LblColor.setText("Color : " 
-			+ self.CboLED.currentText())
+			self.LblColor.setText("Color : " + self.CboLED.currentText())
 			self.LblRepeat.setText("Repeat : " + self.CboRepeat.currentText())
 			self.LblSound.setText("Sound : " + self.CboSound.currentText())
 			print(userSetTime)
 
+	## 하단 설정 정보창 Reset
 	def BtnResetFnc(self):
 		global userSetTime, userSetLED, userSetRepeat, userSetSound
 		userSetTime = userSetLED = userSetRepeat = userSetSound = None
@@ -102,6 +106,7 @@ class WindowClass (QMainWindow, form_class):
 		self.LblRepeat.setText("Repeat : ")
 		self.LblSound.setText("Sound : ")
 
+## 7-segment 숫자 표기
 def findOut(data, sel):
 	for i in range(0,7):
 		GPIO.output(fndSegs[i], fndDatas[data] & (0x01 << i))
@@ -124,10 +129,11 @@ def print_time():
 			findOut(timeList[i],i)
 			time.sleep(0.005)
 
+# 알람 울림 함수
 def LED_blink():
 	GPIO.output(userSetLED, False)
 	for i in range(userSetRepeat):
-		if GPIO.input(switch) == True:
+		if GPIO.input(switch) == True: # switch 버튼 누르면 알람 종료
 			break
 		if userSetSound == "Warning":
 			Buzz.start(30)
@@ -150,12 +156,10 @@ if __name__ =="__main__":
 	myWindow = WindowClass()
 	myWindow.show()
 
-	console_thread = threading.Thread(target=print_time)
+	console_thread = threading.Thread(target=print_time) ## RPi 보드에서 표기 될 7-segments 화면 쓰레드 처리
 	console_thread.daemon = True # 메인 스레드 종료시 함께 종료
 	console_thread.start()
 	sys.exit(app.exec_())
-	GPIO.cleanup()
-
 	try:
 		pass
 	except KeyboardInterrupt:
